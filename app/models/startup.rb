42cd13e71b3625051e6a1fc05adbde95a6d474cb
ayerest <irisiseve@gmail.com>
2019-07-20 14:14:26 -0700

    finished silicon valley code challenge

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,69 @@
+require 'pry'
 class Startup
+    attr_reader :name, :founder, :domain
+    @@all = []
+    @@domains = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+        @@domains << domain
+    end
+
+    def pivot(new_domain, new_name)
+        @domain = new_domain
+        @name = new_name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder_name)
+        self.all.find do |startup|
+            startup.founder == founder_name
+        end
+    end
+
+    def self.domains
+        @@domains
+    end
+
+    def sign_contract(venture_capitalist, investment_type, investment_amt)
+        new_funding_round = FundingRound.new(self, venture_capitalist, investment_type, investment_amt)
+    end
+
+    #helper method to just get a list of all the funding rounds for the particular startup instance
+    def funding_rounds
+        FundingRound.all.select do |funding_round|
+            funding_round.startup == self
+        end
+    end
+
+    def num_funding_rounds
+        self.funding_rounds.length
+    end
+
+    def total_funds
+        sum_funds = 0
+        self.funding_rounds.each do |funding_round|
+            sum_funds += funding_round.investment
+        end
+        sum_funds
+    end
+
+    def investors
+        self.funding_rounds.collect do |funding_round|
+            funding_round.venture_capitalist
+        end.uniq
+    end
+
+    def big_investors
+        self.investors.select do |investor|
+            VentureCapitalist.tres_commas_club.include?(investor)
+        end
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,30 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+startup_one = Startup.new("Test Company", "James Joe", "beepboop")
+startup_two = Startup.new("Hey Whoa", "Cythia Abs", "asdklfjdk")
+startup_three = Startup.new("Sup Co.", "Blah blah", "test test test")
+Startup.find_by_founder("Cythia Abs")
+
+venture_capitalist_one = VentureCapitalist.new("Fluffy", 2000000000)
+venture_capitalist_two = VentureCapitalist.new("Grapes", 50)
+venture_capitalist_three = VentureCapitalist.new("How is this", 1000100100100)
+VentureCapitalist.tres_commas_club
+
+funding_round_one = FundingRound.new(startup_one, venture_capitalist_two, "Angel", 50)
+funding_round_two = FundingRound.new(startup_two, venture_capitalist_one, "Pre-Seed", 30000)
+funding_round_three = FundingRound.new(startup_one, venture_capitalist_three, "Seed", 50000)
+funding_round_four = FundingRound.new(startup_three, venture_capitalist_one, "Series A", 1211150)
+funding_round_four = FundingRound.new(startup_three, venture_capitalist_one, "Series A", 1211150)
+
+
+
+startup_one.sign_contract(venture_capitalist_one, "Pre-Seed", 1000)
+venture_capitalist_two.invested("beepboop")
+venture_capitalist_one.biggest_investment
+venture_capitalist_three.portfolio
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
